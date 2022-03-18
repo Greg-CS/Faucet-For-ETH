e760222a92bed059cdebb2c82375b45fad40f4e4
Greg-CS <gregor.gr20@gmail.com>
2022-03-17 23:06:58 -0400

    3rd commit withdraw transaction working so far

--- src/App.js
+++ src/App.js
@@ -18,20 +18,21 @@ function App() {
   const [account, setAccount] = useState(null)
   const [shouldReload, reload] = useState(false)
 
+  const canConnectToContract = account && web3Api.contract
   const reloadEffect = useCallback(() => reload(!shouldReload), [shouldReload])
 
   const setAccountListener = provider => {
     provider.on("accountsChanged", _ => window.location.reload())
+    provider.on("chainChanged", _ => window.location.reload())
   }
 
   useEffect(() => {
     const loadProvider = async () => {
       const provider = await detectEthereumProvider()
 
-
       if (provider) {
         const contract = await loadContract("Faucet", provider)        
-        setAccountListener(provider )
+        setAccountListener(provider)
         setWeb3Api({
           web3: new Web3(provider),
           provider,
@@ -39,7 +40,7 @@ function App() {
           isProviderLoaded: true
         })
       } else {
-        setWeb3Api({...web3Api, isProviderLoaded: true})
+        setWeb3Api(api => ({...api, isProviderLoaded: true}))
         console.error("Please, install Metamask.")
       }
     }
@@ -91,46 +92,54 @@ function App() {
       <div className="faucet-wrapper">
         <div className="faucet">
           { web3Api.isProviderLoaded ?
-            <div className="is-flex is-align-items-center">          
-            <span>
-              <strong className="mr-2">Account:</strong>
-            </span>
-                { 
-                account ? 
-                <div>{account}</div> : 
-                !web3Api.provider ?
-                <>
-                  <div className="notification is-warning is-size-6 is-rounded">
-                    Wallet is not detected!{` `}
-                    <a target="_blank" href="https://docs.metamask.io">
-                      Install Metamask
-                    </a>
-                  </div>
-                </> :
-                <button
-                className="button is-small"
-                onClick={() => 
-                  web3Api.provider.request({method: "eth_requestAccounts"} )} 
-                >
-                  Connect Wallet
-                </button>
+            <div className="is-flex is-align-items-center">
+              <span>
+                <strong className="mr-2">Account: </strong>
+              </span>
+                { account ?
+                  <div>{account}</div> :
+                  !web3Api.provider ?
+                  <>
+                    <div className="notification is-warning is-size-6 is-rounded">
+                      Wallet is not detected!{` `}
+                      <a
+                      rel="noreferrer" 
+                      target="_blank" 
+                      href="https://docs.metamask.io">
+                        Install Metamask
+                      </a>
+                    </div>
+                  </> :
+                  <button
+                    className="button is-small"
+                    onClick={() =>
+                      web3Api.provider.request({method: "eth_requestAccounts"}
+                    )}
+                  >
+                    Connect Wallet
+                  </button>
                 }
             </div> :
-            <span>Looking for web3...</span>
+            <span>Looking for Web3...</span>
           }
           <div className="balance-view is-size-2 my-4">
             Current Balance: <strong>{balance}</strong> ETH
           </div>
+          { !canConnectToContract &&
+            <i className="is-block">
+              Connect to Ganache
+            </i>
+          }
           <button 
-            disabled={!account}
+            disabled={!canConnectToContract}
             onClick={addFunds}
             className="button is-link mr-2">
-            Donate 1eth
+            Donate 1 eth
           </button>
           <button 
-            disabled={!account}
+            disabled={!canConnectToContract}
             onClick={withdraw}
-            className="button is-primary">Withdraw</button>
+            className="button is-primary">Withdraw 0.1 eth</button>
         </div>
       </div>
     </>
--- src/utils/load-contract.js
+++ src/utils/load-contract.js
@@ -1,5 +1,5 @@
- import contract from "@truffle/contract"
-// var contract = require("@truffle/contract");
+import contract from "@truffle/contract"
+//var contract = require("@truffle/contract");
 
 export const loadContract = async (name, provider) => {
   const res = await fetch(`/contracts/${name}.json`)
@@ -8,7 +8,12 @@ export const loadContract = async (name, provider) => {
   const _contract = contract(Artifact)
   _contract.setProvider(provider)
 
-  const deployedContract = await _contract.deployed()
+  let deployedContract = null
+  try {
+    deployedContract = await _contract.deployed()
+  } catch {
+    console.error("You are connected to the wrong network")
+  }
 
   return deployedContract
 }
